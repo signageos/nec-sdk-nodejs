e68bdf14f4d30172601e1ddcd2fececa481f3882
Michal Artazov <michal@artazov.cz>
2018-10-05 11:27:42 +0200

    log everything from firmware API calls

--- src/API/apiCommand.ts
+++ src/API/apiCommand.ts
@@ -1,11 +1,17 @@
 import * as childProcess from "child_process";
 
+// TODO : maybe log everything somewhere in the cloud somehow
+
 export async function execApiCommand(namespace: string, command: string, args: string = '') {
 	return await new Promise<string>((resolve: (stdout: string) => void, reject: (error: Error) => void) => {
 		const fullCommand = `sudo signageos ${namespace} ${command} ${args}`;
 		childProcess.exec(fullCommand, (error: Error, stdout: string, stderr: string) => {
+			if (stdout) {
+				console.log(fullCommand, stdout);
+			}
+
 			if (stderr) {
-				console.error(stderr);
+				console.error(fullCommand, stderr);
 			}
 
 			if (error) {
@@ -18,5 +24,9 @@ export async function execApiCommand(namespace: string, command: string, args: s
 }
 
 export function spawnApiCommandChildProcess(namespace: string, command: string, args: string[] = []) {
-	return childProcess.spawn('sudo', ['signageos', namespace, command, ...args] as ReadonlyArray<string>);
+	const fullCommand = ['signageos', namespace, command, ...args];
+	const spawnedChildProcess = childProcess.spawn('sudo', fullCommand as ReadonlyArray<string>);
+	spawnedChildProcess.stdout.on('data', (chunk: any) => console.log(fullCommand.join(' '), chunk.toString()));
+	spawnedChildProcess.stderr.on('data', (chunk: any) => console.error(fullCommand.join(' '), chunk.toString()));
+	return spawnedChildProcess;
 }
