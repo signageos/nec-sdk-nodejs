c946102708813fda6a62389f7c05764dd6bb8306
Michal Artazov <michal@artazov.cz>
2018-12-14 10:18:23 +0100

    inject overlay api into sos api instead of an arbitrary global function

--- src/overlay.ts
+++ src/overlay.ts
@@ -4,14 +4,16 @@ const parameters = require('../config/basic_parameters');
 
 let observer: MutationObserver | undefined = undefined;
 
-(window as any).enableOverlay = function () {
-	const bridge = new BridgeClient(parameters.server.bridge_url);
-	observer = new MutationObserver(async (mutations: MutationRecord[]) => {
-		try {
-			await handleMutations(window, bridge, mutations);
-		} catch (error) {
-			console.error('error while handling mutations', error);
-		}
-	});
-	observer.observe(document.body, { childList: true, subtree: true, attributes: true, characterData: true });
+(window as any).sos.overlay = {
+	enable: function () {
+		const bridge = new BridgeClient(parameters.server.bridge_url);
+		observer = new MutationObserver(async (mutations: MutationRecord[]) => {
+			try {
+				await handleMutations(window, bridge, mutations);
+			} catch (error) {
+				console.error('error while handling mutations', error);
+			}
+		});
+		observer.observe(document.body, { childList: true, subtree: true, attributes: true, characterData: true });
+	},
 };
