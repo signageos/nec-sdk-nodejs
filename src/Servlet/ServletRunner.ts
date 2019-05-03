893376022c387ff6d2db57935fb9ad3524164b72
Michal Artazov <michal@artazov.cz>
2019-05-03 17:11:44 +0200

    restart servlet if it exits

--- src/Servlet/ServletRunner.ts
+++ src/Servlet/ServletRunner.ts
@@ -2,6 +2,7 @@ import * as childProcess from 'child_process';
 import IServletRunner from '@signageos/front-display/es6/Servlet/IServletRunner';
 import { IFilePath } from '@signageos/front-display/es6/NativeDevice/fileSystem';
 import IFileSystem from '../FileSystem/IFileSystem';
+import wait from '@signageos/lib/dist/Timer/wait';
 
 // TODO manage forked processes, add logic to be able to cancel them, etc.
 
@@ -11,8 +12,22 @@ export default class ServletRunner implements IServletRunner {
 
 	public async run(entryPoint: IFilePath, env?: { [p: string]: any }): Promise<void> {
 		const entryPointAbsolutePath = this.fileSystem.getAbsolutePath(entryPoint);
-		childProcess.fork(entryPointAbsolutePath, [] as ReadonlyArray<string>, {
+		const servletProcess = childProcess.fork(entryPointAbsolutePath, [] as ReadonlyArray<string>, {
 			env: env as NodeJS.ProcessEnv,
+		});
+		servletProcess.once('close', async (code: number, signal: string | null) => {
+			if (signal) {
+				console.warn(`servlet process has exited with code ${code} after receiving signal ${signal}`);
+			} else {
+				console.warn(`servlet process has exited with code ${code}`);
+			}
+
+			const TIMEOUT_BEFORE_RESTART = 1e3;
+			await wait(TIMEOUT_BEFORE_RESTART);
+			await this.run(entryPoint, env);
+		});
+		servletProcess.on('error', (error: Error) => {
+			console.error('servlet error', error);
 		});
 	}
 }
