1604935ca3d182cdbf99b7a6002888a4a852e7ee
Michal Artazov <michal@artazov.cz>
2020-04-02 14:50:26 +0200

    remove obsolete settermblank service
    it was a first attempt to hide the console and the raspberry pi logos during boot but we have better tools for it now and this does nothing

--- tools/build-overlay-and-cache.sh
+++ tools/build-overlay-and-cache.sh
@@ -101,7 +101,6 @@ create_runlevel factory_reset
 create_runlevel overwrite_system
 
 # configure signageos services
-enable_service settermblank sysinit
 enable_service set-ondemand-cpu-governor boot
 enable_service signageos-clock boot
 enable_service signageos-firstboot default
